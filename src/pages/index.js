df32f1a3218af8bd139414a6acbc1df9421872eb
Sn0w <clement.ozor@epitech.eu>
2022-05-19 22:58:51 +0200

    [release/2.0.0] => Init gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import {Helmet} from "react-helmet";
+
+const IndexPage = () => {
+      return (
+          <>
+              <Helmet>
+                  <title>Clément OZOR, étudiant développeur | ClémentOS</title>
+              </Helmet>
+          </>
+      )
+}
+
+export default IndexPage
