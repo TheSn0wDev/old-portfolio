fbefc4bb28ef8577902a7374ca848fbe0fe44204
Sn0w <clement.ozor@epitech.eu>
2022-05-25 21:11:18 +0200

    [release/2.0.0] => Add header & favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `Portfolio`,
+    title: `Clément OZOR, étudiant développeur | ClémentOS`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"]
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/favicon.png',
+      },
+    },
+    "gatsby-plugin-sass", "gatsby-plugin-react-helmet"
+  ],
 };
---
+++ src/components/MainContainer.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import OSHeader from "./OSHeader";
+
+const MainContainer = () => {
+    return (<>
+        <OSHeader />
+        <h1 id={"backgroundTitle"}>ClementOS</h1>
+    </>);
+}
+
+export default MainContainer;
