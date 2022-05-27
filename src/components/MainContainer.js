cbbef2c04c816efcb339e09af30c9d16a75d43e5
Sn0w <clement.ozor@epitech.eu>
2022-05-28 01:10:43 +0200

    [release/2.0.0] => Add skills content

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -5,7 +5,14 @@ import OSWindow from "./OSWindow";
 import NavbarCategory from "./NavbarCategory";
 import NavbarLink from "./NavbarLink";
 import OSTab from "./OSTab";
-import {faAlignLeft, faGraduationCap} from "@fortawesome/free-solid-svg-icons";
+import {
+    faAlignLeft, faBrush,
+    faCubes,
+    faDatabase,
+    faGraduationCap,
+    faLaptopCode, faListCheck,
+    faTerminal
+} from "@fortawesome/free-solid-svg-icons";
 
 const qualifications = [
     {
@@ -57,7 +64,6 @@ const MainContainer = () => {
             <NavbarCategory title={"Général"}/>
             <NavbarLink title={"Présentation"} icon={faAlignLeft} toOpen={"presentation"} active={"true"} window={"about"} />
             <NavbarLink title={"Études"} icon={faGraduationCap} toOpen={"studies"} active={"false"} window={"about"} />
-            {/*<NavbarLink title={"Studies"} toOpen={"about2"} active={"false"} window={"about"}/>*/}
         </>}>
             <OSTab id={"tab-presentation"} title={"About"}>
                 <div className={"contentContainer"}>
@@ -72,17 +78,204 @@ const MainContainer = () => {
             <OSTab id={"tab-studies"} title={"About"}>
                 {itemsQualifications}
             </OSTab>
-            <OSTab id={"tab-about2"} title={"About"}>
-                test2
+        </OSWindow>
+        <OSWindow title={"Compétences"} id={"skills"} navbar={<>
+            <NavbarCategory title={"Développement Web"}/>
+            <NavbarLink title={"Front-end"} icon={faBrush} toOpen={"front-end"} active={"true"} window={"skills"} />
+            <NavbarLink title={"Back-end"} icon={faCubes} toOpen={"back-end"} active={"false"} window={"skills"} />
+            <NavbarLink title={"Base de données"} icon={faDatabase} toOpen={"database"} active={"false"} window={"skills"} />
+
+            <NavbarCategory title={"Programmation"}/>
+            <NavbarLink title={"Langages"} icon={faTerminal} toOpen={"languages"} active={"false"} window={"skills"} />
+
+            <NavbarCategory title={"Autres"}/>
+            <NavbarLink title={"IDE"} icon={faLaptopCode} toOpen={"ide"} active={"false"} window={"skills"} />
+            <NavbarLink title={"Gestion de projet"} icon={faListCheck} toOpen={"project-management"} active={"false"} window={"skills"} />
+        </>}>
+            <OSTab id={"tab-front-end"} >
+                <div className={"contentContainer"}>
+                    <h5>HTML5</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "90%"}}/>
+                    </div>
+
+                    <h5>CSS3</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "90%"}}/>
+                    </div>
+
+                    <h5>JavaScript</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "75%"}}/>
+                    </div>
+
+                    <h5>SCSS</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "70%"}}/>
+                    </div>
+
+                    <h5>React</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "65%"}}/>
+                    </div>
+
+                    <h5>Tailwind CSS</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "60%"}}/>
+                    </div>
+
+                    <h5>Bootstrap</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "30%"}}/>
+                    </div>
+
+                    <h5>VueJS</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "10%"}}/>
+                    </div>
+                </div>
+            </OSTab>
+            <OSTab id={"tab-back-end"} >
+                <div className={"contentContainer"}>
+                    <h5>Symfony</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "85%"}}/>
+                    </div>
+
+                    <h5>PHP</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "70%"}}/>
+                    </div>
+
+                    <h5>NodeJS</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "60%"}}/>
+                    </div>
+
+                    <h5>ExpressJS</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "60%"}}/>
+                    </div>
+                </div>
+            </OSTab>
+            <OSTab id={"tab-database"} >
+                <div className={"contentContainer"}>
+                    <h5>MySQL</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "90%"}}/>
+                    </div>
+
+                    <h5>MongoDB</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "40%"}}/>
+                    </div>
+
+                    <h5>PostgreSQL</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "10%"}}/>
+                    </div>
+                </div>
+            </OSTab>
+            <OSTab id={"tab-languages"} >
+                <div className={"contentContainer"}>
+                    <h5>C</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "95%"}}/>
+                    </div>
+                </div>
+
+                <div className={"contentContainer"}>
+                    <h5>C++</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "90%"}}/>
+                    </div>
+                </div>
+
+                <div className={"contentContainer"}>
+                    <h5>Lua</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "80%"}}/>
+                    </div>
+                </div>
+
+                <div className={"contentContainer"}>
+                    <h5>Python</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "70%"}}/>
+                    </div>
+                </div>
+
+                <div className={"contentContainer"}>
+                    <h5>Ruby</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "40%"}}/>
+                    </div>
+                </div>
+
+                <div className={"contentContainer"}>
+                    <h5>Haskell</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "20%"}}/>
+                    </div>
+                </div>
+            </OSTab>
+            <OSTab id={"tab-ide"} >
+                <div className={"contentContainer"}>
+                    <h5>CLion</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "90%"}}/>
+                    </div>
+
+                    <h5>Webstorm</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "90%"}}/>
+                    </div>
+
+                    <h5>Visual Studio Code</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "70%"}}/>
+                    </div>
+
+                    <h5>IntelliJ</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "65%"}}/>
+                    </div>
+
+                    <h5>PhpStorm</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "60%"}}/>
+                    </div>
+
+                    <h5>PyCharm</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "50%"}}/>
+                    </div>
+                </div>
+            </OSTab>
+            <OSTab id={"tab-project-management"} >
+                <div className={"contentContainer"}>
+                    <h5>Git</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "90%"}}/>
+                    </div>
+
+                    <h5>GitHub</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "80%"}}/>
+                    </div>
+
+                    <h5>Trello</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "80%"}}/>
+                    </div>
+
+                    <h5>Confluence</h5>
+                    <div className={"progressBarContainer"}>
+                        <div className={"progressBar"} style={{width: "40%"}}/>
+                    </div>
+                </div>
             </OSTab>
         </OSWindow>
-
-        <OSWindow title={"Compétences"} id={"skills"} navbar={<>
-            <NavbarCategory title={"Main"}/>
-            <NavbarLink title={"Studies"} toOpen={"about"} active={"true"} window={"projects"}/>
-            <NavbarLink title={"Studies"} toOpen={"about"} active={"false"} window={"projects"}/>
-            <NavbarLink title={"Studies"} toOpen={"about"} active={"false"} window={"projects"}/>
-        </>}/>
         <OSWindow title={"Projets"} id={"projects"} navbar={<>
             <NavbarCategory title={"Main"}/>
             <NavbarLink title={"Studies"} toOpen={"about"} active={"true"} window={"projects"}/>
