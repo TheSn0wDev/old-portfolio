b7120cc370671bbf0b078d6c7f5f8154ead35c7e
Sn0w <clement.ozor@epitech.eu>
2022-05-28 13:05:43 +0200

    [main] => Fix body background

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -5,8 +5,6 @@ import OSWindow from "./OSWindow";
 import NavbarCategory from "./NavbarCategory";
 import NavbarLink from "./NavbarLink";
 import OSTab from "./OSTab";
-import background from "../images/background.jpg";
-
 import {
     faAlignLeft, faBrush,
     faCubes,
