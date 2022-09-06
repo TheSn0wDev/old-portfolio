eeb01fb45d7c9de63de0ed4b2f4f455f663a6742
Sn0w <clement.ozor@protonmail.com>
2022-09-06 20:30:51 +0200

    Updates

--- src/components/NavbarCategory.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-const NavbarCategory = (props) => {
-    return (<h3 className={"NavbarCategory"}>{props.title}</h3>);
-}
-
-export default NavbarCategory;
--- src/components/OSTab.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-const OSTab = (props) => {
-    return (<div id={props.id} className={"OSWindowTab"} data-show={"true"}>
-        {props.children}
-    </div>);
-}
-
-export default OSTab;
